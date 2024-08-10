f73d1cb97a50f269c5998702ce6a804c60f45eb0
Noah Schill <noahrschill@gmail.com>
2024-08-10 16:21:21 -0700

    spotify mocks

--- tests/fixtures/spotify_client.py
+++ tests/fixtures/spotify_client.py
@@ -1,12 +1,11 @@
 """Fixtures for the Spotify client."""
 
-from unittest.mock import patch
-
 import pytest
-
+from unittest.mock import patch
 
 @pytest.fixture
 def mock_spotify_client():
     """Mock the Spotify client."""
-    with patch("deepcuts_api.spotify.spotipy.Spotify") as mock_spotify:
-        yield mock_spotify
+    with patch("deepcuts_api.spotify.spotipy.Spotify") as MockSpotify:
+        mock_instance = MockSpotify.return_value
+        yield mock_instance
