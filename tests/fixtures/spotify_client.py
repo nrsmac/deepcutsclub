14b39ca674f955c10b660af51dac385bf2576c36
Noah Schill <noahrschill@gmail.com>
2024-08-10 16:33:44 -0700

    defined tests for apple music

--- tests/fixtures/spotify_client.py
+++ tests/fixtures/spotify_client.py
@@ -1,11 +1,13 @@
 """Fixtures for the Spotify client."""
 
-import pytest
 from unittest.mock import patch
+
+import pytest
+
 
 @pytest.fixture
 def mock_spotify_client():
     """Mock the Spotify client."""
-    with patch("deepcuts_api.spotify.spotipy.Spotify") as MockSpotify:
-        mock_instance = MockSpotify.return_value
+    with patch("deepcuts_api.spotify.spotipy.Spotify") as mock_spotify:
+        mock_instance = mock_spotify.return_value
         yield mock_instance
